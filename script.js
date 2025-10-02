31afc3947a26510b7505dd4dfac17c0f48326fa2
caraka-says <akbarcaraka424@gmail.com>
2025-10-02 12:15:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-const tombolSapa = document.getElemenById('sapaButton');
-tombolSapa.addEventListener('click',function(){alert('forza milan!');
-                                              });
+const tombolSapa = document.getElementById('sapaButton');
+tombolSapa.addEventListener('click', function() {
+  alert('Halo! Terima kasih sudah berkunjung!');
+});
